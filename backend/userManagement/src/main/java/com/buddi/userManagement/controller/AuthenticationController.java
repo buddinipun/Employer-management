846b23f26e43c2303798c27fd67b2aa83f7e3bff
Buddhika Nipun <buddinipun@gmail.com>
2024-12-19 11:20:24 +0530

    jwt implementation
    fix issue on jwt dependancy conflits and implement jwtutil class

---
+++ backend/userManagement/src/main/java/com/buddi/userManagement/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.buddi.userManagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticationController {
+
+}
